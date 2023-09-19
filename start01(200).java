b74627353ac4c37173dba6ae22fedbb76b82b193
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-09-20 00:31:52 +0530

    Create start01(200).java

---
+++ start01(200).java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+	    System.out.println(n);// taking iput from java 
+
+		
+	}
+}
