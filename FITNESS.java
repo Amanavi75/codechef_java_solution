162446570414d51c692d189d8eff43fc3f77dd6e
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-09-24 10:05:57 +0530

    Create FITNESS.java

---
+++ FITNESS.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i=1;i<=t ;i++ ){
+		    int x = sc.nextInt();
+		    System.out.println((2*x)*5);
+		}
+	}
+}
