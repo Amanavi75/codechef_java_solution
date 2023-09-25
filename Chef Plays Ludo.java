da7fb9b876ab1b4bd8f742718a455234c1b59a5e
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-09-25 22:13:17 +0530

    Create Chef Plays Ludo.java

---
+++ Chef Plays Ludo.java
@@ -0,0 +1,18 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		
+		while(t-->0){
+		    int x = scn.nextInt();
+		    if(x==6){
+		        System.out.println("yes");
+		    }else {
+		        System.out.println("no");
+		    }
+		}
+	}
+}
