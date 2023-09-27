fb25365cb0d9f33b11898e97f5158d82dcc2debe
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-09-27 13:55:59 +0530

    Create how many unattempted.java

---
+++ how many unattempted.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scn = new Scanner(System.in);
+		int x = scn.nextInt();
+		int y = scn.nextInt();
+		int unattempted = x - y;
+	     System.out.println( unattempted);
+		
+	}
+}
