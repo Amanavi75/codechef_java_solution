de5a812a1d1b7e513738ccd83fe43831774debd4
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-09-27 14:07:18 +0530

    Create determine the score.java

---
+++ determine the score.java
@@ -0,0 +1,22 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner sc = new Scanner(System.in);
+	  int T = sc.nextInt();
+	  
+	  for (int Test = 0 ; Test < T ; Test++)
+	  {
+	      int X = sc.nextInt();
+	      int N = sc.nextInt();
+	      
+	      
+	      
+	      int Z = X/10;
+	      
+	      int Y = Z * N;
+	      
+	      System.out.println(Y);
+	  }
+	}
+}
