c8becbd35e3e1fe6d72eceffa63da6703d70c24e
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-09-28 22:14:50 +0530

    Create off Bye one.java

---
+++ off Bye one.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int count = a+b;
+		System.out.println((count)*10 + 1);
+	}
+}
