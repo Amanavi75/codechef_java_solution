2fc02ee49aecf12762552e3bb795d6a610a1ae6f
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-09-29 23:11:09 +0530

    Create donation drive.java

---
+++ donation drive.java
@@ -0,0 +1,15 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    System.out.println(x-y);
+		}
+	}
+}
