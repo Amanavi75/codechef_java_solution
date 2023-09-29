264a9e8f5ab7e0f8ba1be563381017ea6bf79fbc
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-09-29 23:12:31 +0530

    Create sports section.java

---
+++ sports section.java
@@ -0,0 +1,16 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		
+		if(n > 7 && n <= 10){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+	}
+}
