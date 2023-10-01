79165290a10b8f2798d4c76460230f7fd3bb333f
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-01 23:38:05 +0530

    Create puzzle hunt.java

---
+++ puzzle hunt.java
@@ -0,0 +1,18 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		
+		  int x=sc.nextInt();
+		  if(x>=6 && x<=8) {
+		    System.out.println("yes");
+		    
+		  }
+		  else{
+		    System.out.println("no");
+		  }
+		
+	}
+}
