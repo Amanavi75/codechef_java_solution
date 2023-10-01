4b8dd318142cd6acca85faa7e8139deec2b2e6ea
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-01 23:40:15 +0530

    Create reach the target.java

---
+++ reach the target.java
@@ -0,0 +1,15 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ 
+	    Scanner s= new Scanner(System.in); 
+	    int n= s.nextInt(); 
+	    while(n>0){ 
+	        int a=s.nextInt(); 
+	        int b= s.nextInt();
+	        System.out.println(a-b); 
+	        n--; 
+	    }
+		// your code goes here
+	}
+}
