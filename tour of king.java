ea77acef471eff3dc6de56861934a641fbf497bc
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-02 23:48:23 +0530

    Create tour of king.java

---
+++ tour of king.java
@@ -0,0 +1,14 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int m=sc.nextInt();
+		    System.out.println(n*5+m*7);
+		}
+	}
+}
