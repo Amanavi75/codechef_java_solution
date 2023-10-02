076d8b96c7e3ac58985942dd64a42f3c79d51299
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-02 23:51:18 +0530

    Create 2000.java

---
+++ 2000.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int  t=sc.nextInt();
+		System.out.println((t*2000)/500);
+	}
+}
