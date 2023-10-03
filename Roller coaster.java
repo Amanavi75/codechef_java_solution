759f4b975095faadf4fe2e8cd2fe01dcaaba1efe
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-03 22:44:00 +0530

    Create Roller coaster.java

---
+++ Roller coaster.java
@@ -0,0 +1,20 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t!=0){
+		    int x =s.nextInt();
+		    int h = s.nextInt();
+		    if(x>=h){
+		        System.out.println("YES");
+		    }else{
+		        System.out.println("NO");
+		    }
+		    
+		    
+		    t--;
+	}
+}
