4bd90de13bc12b2fe9b0b2f84b0717859e019b73
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-03 22:46:45 +0530

    Create codechef.java

---
+++ codechef.java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(2*n);
+	}
+}
