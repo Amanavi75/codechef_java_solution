49751fa26d10d8bc565030e2548a97c8105e011a
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-03 22:55:01 +0530

    Create odd sum pairs.java

---
+++ odd sum pairs.java
@@ -0,0 +1,18 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t  = sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x  = sc.nextInt();
+		    int y  = sc.nextInt();
+		    int z  = sc.nextInt();
+		    if((x+y)%2==1 || (y+z)%2==1 || (x+z)%2==1)
+		    System.out.println("YES");
+		    else System.out.println("NO");
+		}
+	}
+}
