30e127300a47fa35ee3e0e9aa6b6280e1106b3aa
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-04 21:48:24 +0530

    Create x jumps.java

---
+++ x jumps.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+int t=s.nextInt();
+while( t-- >0){
+    int i=s.nextInt();int j=s.nextInt();
+     System.out.println(i/j+i%j);
+	}
+	}
+}
