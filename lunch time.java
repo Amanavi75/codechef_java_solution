02ae64270b14e5be55f431de8a0e5c6614befc7a
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-05 20:20:02 +0530

    Create lunch time.java

---
+++ lunch time.java
@@ -0,0 +1,18 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t,n;
+		t=sc.nextInt();
+		while(t-- >0){
+		    n=sc.nextInt();
+		    if(n>=1&&n<=4){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
