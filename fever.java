983d392b5b51fac3bba49ce0d324e5c107801d64
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-05 20:21:32 +0530

    Create fever.java

---
+++ fever.java
@@ -0,0 +1,17 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s =new Scanner(System.in);
+	    int n =s.nextInt();
+	    while(n>0){
+	        int a = s.nextInt();
+	        if(a>98)
+	        System.out.println("YES");
+	        else 
+	        System.out.println("No");
+	        n--;
+	    }
+		// your code goes here
+	}
+}
