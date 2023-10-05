375fa51bfdf240ec1c7acadd422a689526d20f7a
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-05 20:25:19 +0530

    Create maximum submission.java

---
+++ maximum submission.java
@@ -0,0 +1,14 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    int a = (x*2);
+		    System.out.println(a);
+		}
+	}
+}
