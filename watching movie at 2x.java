ee371acf8f759c72cadbfc3dd113e77360cedbf0
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-08 22:15:23 +0530

    Create watching movie at 2x.java

---
+++ watching movie at 2x.java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		System.out.println(n-m/2);
+	}
+}
