ea825cc889e2383b1a47820db5c71435ed1ca412
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-09 23:31:10 +0530

    Create chef on date.java

---
+++ chef on date.java
@@ -0,0 +1,23 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+		    int a=s.nextInt();
+		    int b=s.nextInt();
+		    if(a>=b)
+		    {
+		        System.out.println("Yes");
+		    }
+		    else
+		    {
+		        System.out.println("No");
+		    }
+		    
+		}
+	}
+}
