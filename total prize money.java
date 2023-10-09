12b95ae2a978cda9d8bdab8d83dd4a1ef271dab4
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-09 23:31:52 +0530

    Create total prize money.java

---
+++ total prize money.java
@@ -0,0 +1,14 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{// your code goes here
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t!=0){
+		    int x = s.nextInt();
+		    int y = s.nextInt();
+		    System.out.println((10*x) + (y*90));
+		    
+		    t--;
+	}
+}
