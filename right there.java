e1a4aee1bf006c5fdc8aced0a509af69e1ca993a
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-09 23:32:58 +0530

    Create right there.java

---
+++ right there.java
@@ -0,0 +1,18 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+	  int t=sc.nextInt();
+	  for(int i=0;i<t;i++)
+	  {
+	      int N=sc.nextInt();
+	      int X=sc.nextInt();
+	      if(N<=X)
+	         System.out.println("yes");
+	       else
+	          System.out.println("No");
+	  }
+	}
+}
+
